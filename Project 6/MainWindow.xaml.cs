09b769ddc0fb05910f651ee1cee682e0ca4f5405
SRGibson89 <s-gibson89@live.co.uk>
2017-01-17 13:21:34 +0000

    Project 6 and 7 from Project Euler
    Projects 6 and 7 from Project Euler for my Software Development 2 course

---
+++ Project 6/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            int sq = 0, addsq = 0;
+            for (int num = 1; num <= 100; num ++)
+            {
+                sq = (num * num);
+                addsq = sq + addsq; 
+            }
+
+            int add = 0, sqadd = 0,count = 1;
+
+            while (count <= 100)
+            {
+                add = count + add;
+                count++;
+                sqadd = (add * add);
+                
+            }
+            int answer = (sqadd - addsq);
+            
+                lblDisplay.Content = ("The answer is " + answer);
+        }
+    }
+}
---
+++ Project 7/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static Boolean isPrime(int n)
+        {
+            if (n<= 1)
+            {
+                return false;
+            }
+            if (n<=3)
+            {
+                return true;
+            }
+            if ((n%2==0) || (n % 3 == 0))
+            {
+                return false;
+            }
+            int i = 5;
+            while (i*i<=n)
+            {
+                if ((n%i==0) ||(n % (i+2) == 0))
+                {
+                    return false;
+
+                }
+                i = i + 6;
+            }
+            return true;
+        }
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int prime = 0;
+            
+            int count = 0;
+            for (count = 0; prime<10001 ;count++)
+            {
+                if (isPrime(count)==true)
+                {
+                    prime++;
+                    
+                }
+            }
+            lblDisplay.Content = (" The " + prime + "st Prime number is  " + (count-1));
+        }
+    }
+}
